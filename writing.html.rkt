90f6bd0e96689d0f9556e810145846234b050430
gregr <greg.weiqi@gmail.com>
2015-05-20 14:09:11 -0400

    Define writing-feed

--- writing.html.rkt
+++ writing.html.rkt
@@ -1,6 +1,7 @@
 #lang racket
 (provide
   writing
+  writing-feed
   writing-names
   writing-pages
   )
@@ -8,15 +9,24 @@
 (require
   "common.rkt"
   "static-site.rkt"
+  gregr-misc/list
+  gregr-misc/sugar
   racket/runtime-path
   )
 
-(define writing-names
-  '(
-    creative-workflow
-    reluctant-bash-scripting
-    staying-sane-writing-software
+(define names-dates
+  '((creative-workflow ("2015-03-17" "2015-03-18"))
+    (reluctant-bash-scripting ("2014-10-25" "2014-12-07"))
+    (staying-sane-writing-software ("2014-06-22" "2014-06-22"))
     ))
+(define writing-names (forl (list name _) <- names-dates
+                            name))
+(define published-updated (forl (list _ pub-upd) <- names-dates
+                                pub-upd))
+
+(define home-href (string-append "http://www." domain))
+(define (writing-href name)
+  (string-append home-href "/writing/" (symbol->string name)))
 
 (define writing-nodes
   (for/list ((name writing-names))
@@ -68,3 +78,13 @@
   (for/list ((name writing-names) (result writing-results))
     (let ((desc (car result)) (xexpr (cadr result)))
       (list name desc xexpr))))
+
+(define writing-feed
+  (atom-feed
+    (string-append home-href "/" "writing.xml")
+    home-href
+    (forl
+      (list (list name desc xexpr)
+            (list published updated)) <- (zip* writing-pages published-updated)
+      (atom-entry desc (symbol->string name) (writing-href name)
+                  published updated desc))))
